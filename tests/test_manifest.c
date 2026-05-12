7e73b534fb46e56a670119de4f9af0c8b808e979
Mika Tuupola <tuupola@appelsiini.net>
2026-05-12 20:39:15 +0300

    Shorten test name

--- tests/test_manifest.c
+++ tests/test_manifest.c
@@ -10,7 +10,7 @@ TEST test_init_null_pointer(void) {
     PASS();
 }
 
-TEST test_init_success(void) {
+TEST test_init(void) {
     drip_manifest_t message;
 
     int rc = drip_manifest_init(&message);
@@ -19,22 +19,19 @@ TEST test_init_success(void) {
     PASS();
 }
 
-TEST test_get_vnb_null_ptr(void)
-{
+TEST test_get_vnb_null_ptr(void) {
     uint32_t result = drip_manifest_get_vnb(NULL);
     ASSERT_EQ(0, result);
     PASS();
 }
 
-TEST test_set_vnb_null_ptr(void)
-{
+TEST test_set_vnb_null_ptr(void) {
     int rc = drip_manifest_set_vnb(NULL, 123);
     ASSERT_EQ(DRIP_ERROR_NULL_POINTER, rc);
     PASS();
 }
 
-TEST test_set_and_get_vnb(void)
-{
+TEST test_set_and_get_vnb(void) {
     drip_manifest_t message;
     uint32_t value = 123456789;
 
@@ -47,7 +44,7 @@ TEST test_set_and_get_vnb(void)
 
 SUITE(manifest_suite) {
     RUN_TEST(test_init_null_pointer);
-    RUN_TEST(test_init_success);
+    RUN_TEST(test_init);
     RUN_TEST(test_get_vnb_null_ptr);
     RUN_TEST(test_set_vnb_null_ptr);
     RUN_TEST(test_set_and_get_vnb);
