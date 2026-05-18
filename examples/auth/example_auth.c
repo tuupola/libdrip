262c7cdc084e36db0fffe949667b01c28207a7a2
Mika Tuupola <tuupola@appelsiini.net>
2026-05-18 13:54:55 +0300

    Build a Message Pack from a Manifest

--- examples/auth/example_auth.c
+++ examples/auth/example_auth.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 #include "drip/manifest.h"
+#include "rid/auth.h"
+#include "rid/message_pack.h"
 
 static void hexdump(const void *data, size_t size) {
     const uint8_t *bytes = (const uint8_t *)data;
@@ -16,15 +18,59 @@ static void hexdump(const void *data, size_t size) {
     printf("\n\n");
 }
 
+static void hexstring(const void *data, size_t size) {
+    const uint8_t *p = data;
+    while (size--) {
+        printf("%02x", *p++);
+    }
+    printf("\n");
+}
+
 int main(void) {
     drip_manifest_t manifest;
     uint8_t encoded[256];
     size_t encoded_length;
+    rid_auth_t auth;
+    uint8_t signature[256];
+    rid_message_pack_t pack;
 
     drip_manifest_init(&manifest);
     drip_manifest_encode(&manifest, encoded, sizeof(encoded), &encoded_length);
 
+    printf("Manifest:\n");
     hexdump(encoded, encoded_length);
+
+    rid_auth_init(&auth);
+    rid_auth_set_type(&auth, RID_AUTH_TYPE_SPECIFIC_METHOD);
+    rid_auth_set_unixtime(&auth, (uint32_t)time(NULL));
+    rid_auth_set_signature(&auth, encoded, encoded_length);
+    rid_auth_get_signature(&auth, signature, sizeof(signature));
+
+    printf("Auth type:  %d\n", rid_auth_get_type(&auth));
+    printf("Timestamp:  %u\n", rid_auth_get_timestamp(&auth));
+    printf("Length:     %d bytes\n\n", rid_auth_get_length(&auth));
+    printf("Page count: %d\n", rid_auth_get_page_count(&auth));
+
+    printf("\nSignature:\n");
+    hexdump(signature, encoded_length);
+
+    printf("Auth pages:\n");
+    uint8_t page_count = rid_auth_get_page_count(&auth);
+    for (uint8_t i = 0; i < page_count; i++) {
+        printf("Page %d:\n", i);
+        if (i == 0) {
+            hexdump(&auth.page_0, sizeof(auth.page_0));
+        } else {
+            hexdump(&auth.page_x[i - 1], sizeof(auth.page_x[0]));
+        }
+    }
+
+    rid_message_pack_init(&pack);
+    rid_message_pack_set_auth(&pack, &auth);
+
+    printf("Message pack:\n");
+    hexdump(&pack, rid_message_pack_get_size(&pack));
+    hexstring(&pack, rid_message_pack_get_size(&pack));
 
     return 0;
 }
