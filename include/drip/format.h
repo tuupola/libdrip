5a5c148c35dcccb3932384c561c5de2cd420e7f9
Mika Tuupola <tuupola@appelsiini.net>
2026-09-07 10:43:03 +0300

    Add enum for Specific Session ID types

--- include/drip/format.h
+++ include/drip/format.h
@@ -53,4 +53,16 @@ typedef enum {
     DRIP_SAM_TYPE_FRAME = 0x04,
 } drip_sam_type_t;
 
+/**
+ * @brief Specific Session ID (SSI) Type values per RFC 9374.
+ *
+ * First byte of the 20-byte UAS ID when ID type is 4 (Specific Session ID).
+ *
+ * @see https://www.rfc-editor.org/rfc/rfc9374.html#section-4
+ */
+typedef enum {
+    DRIP_SSI_TYPE_DRIP_ENTITY_ID = 0x01,
+    DRIP_SSI_TYPE_IEEE_1609_2_HASHED_ID8 = 0x02,
+} drip_ssi_type_t;
+
 #endif
