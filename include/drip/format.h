05bf9b86de67bb193c4fbf85aee5a3f5bbd7a3a6
Mika Tuupola <tuupola@appelsiini.net>
2026-09-15 10:53:58 +0000

    Add enum for Specific Session ID (SSI) types (#38)
    Reviewed-on: https://forgejo.internal/tuupola/libdrip/pulls/38

--- include/drip/format.h
+++ include/drip/format.h
@@ -28,4 +28,17 @@ typedef enum {
     DRIP_SAM_TYPE_FRAME = 0x04,
 } drip_sam_type_t;
 
+/**
+ * @brief Specific Session ID (SSI) Type values per RFC 9374.
+ *
+ * First byte of the 20-byte Remote ID UAS ID when ID type is 4 (SSI).
+ *
+ * @see https://www.rfc-editor.org/rfc/rfc9374.html#section-4
+ * @see https://www.librid.org/latest/api/basic_id/#enum-rid_basic_id_type_t
+ */
+typedef enum {
+    DRIP_SSI_TYPE_DRIP_ENTITY_ID = 0x01,
+    DRIP_SSI_TYPE_IEEE_1609_2_HASHED_ID8 = 0x02,
+} drip_ssi_type_t;
+
 #endif
